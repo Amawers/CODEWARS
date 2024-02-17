e02c6fcd0e7f7c79b0db27f87fb647b543199e37
Amawers <dacoroon.christianjericho@gmail.com>
2024-02-18 00:35:38 +0800

    SOLVED: Find Maximum and Minimum Values of a List

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
-function basicOp($op, $val1, $val2)
+function maximum($array)
 {
-  if($op == "+"){
-    return $val1 + $val2;
-  }else if($op == "-"){
-    return $val1 - $val2;
-  }else if($op == "*"){
-    return $val1 * $val2;
-  }else{
-    return $val1 / $val2;
-  }
+    return max($array);
 }
-basicOp ('+',5, 5);
+function minimum($array)
+{
+    return min($array);
+}
+maximum([1, 2, 3]);
