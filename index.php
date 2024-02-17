2093dd65c9d459bfb54806a91c985bc6585e919f
Amawers <dacoroon.christianjericho@gmail.com>
2024-02-17 23:58:37 +0800

    SOLVED: Basic math calculator

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-function rpc ($p1, $p2) {
-    if($p1 == "scissors" && $p2 == "paper" || $p1 == "paper" && $p2 == "rock" || $p1 == "rock" && $p2 == "scissors"){
-        return "Player 1 won!";
-    }else if($p1 == "paper" && $p2 == "scissors" || $p1 == "rock" && $p2 == "paper" || $p1 == "scissors" && $p2 == "rock"){
-        return "Player 2 won!";
-    }else{
-        return "Draw!";
-    }
+function basicOp($op, $val1, $val2)
+{
+  if($op == "+"){
+    return $val1 + $val2;
+  }else if($op == "-"){
+    return $val1 - $val2;
+  }else if($op == "*"){
+    return $val1 * $val2;
+  }else{
+    return $val1 / $val2;
+  }
 }
-rpc ("paper", "paper");
+basicOp ('+',5, 5);
