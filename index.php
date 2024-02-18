904f3c425a7cc28390b66cf75d21626256c8f65c
Amawers <dacoroon.christianjericho@gmail.com>
2024-02-18 15:43:55 +0800

    SOLVED: Jaden Casing Strings

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
-function quarterOf($month)
+function toJadenCase(string $string): string
 {
-    switch ($month) {
-        case in_array($month, range(1, 3)):
-            return 1;
-        case in_array($month, range(4, 6)):
-            return 2;
-        case in_array($month, range(7, 9)):
-            return 3;
-        case in_array($month, range(10, 12)):
-            return 4;
-    }
+    return ucwords($string);
 }
-$returned = quarterOf(11);
-echo $returned;
+toJadenCase("i love u why stalk my github?");
