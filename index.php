ee2a2da901e22e6d01f3ad93ea04df8029dbf511
Amawers <dacoroon.christianjericho@gmail.com>
2024-02-19 21:56:17 +0800

    SOLVED: You only need one - Beginner

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
-function getMiddle($text)
+function solution($a, $x)
 {
-    $split = str_split($text);
-    $check = count($split);
-    if($check % 2){
-        $middle = floor($check / 2);
-        return $split[$middle];
-    }else{
-        $first = ($check / 2) - 1;
-        $second = $check / 2;
-
-        return $split[$first] . $split[$second];
-    }
+    return in_array($x, $a);
 }
-getMiddle("test");
+solution(['a', 'b', 'c'], 'a');
