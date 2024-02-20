e82fd932161253d1032bba663ee6b9f7a61ef0b4
Amawers <dacoroon.christianjericho@gmail.com>
2024-02-20 22:01:58 +0800

    SOLVED: A Needle in the Haystack

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
-function nbYear($p0, $percent, $aug, $p)
+function findNeedle($haystack)
 {
-    $count = 0;
-    while ($p0 < $p) {
-        $p0 = $p0 + floor($p0 * ($percent / 100)) + $aug;
-        $count++;
-    }
-    return $count;
+    return "found the needle at position " . array_search("needle", $haystack);
 }
-nbYear(1500, 5, 100, 5000);
+findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]);
