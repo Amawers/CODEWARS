a9943deb8cd9f42bbc5cac7df26b284a12395d5b
Amawers <dacoroon.christianjericho@gmail.com>
2024-02-22 09:13:48 +0800

    SOLVED: Beginner - Lost Without a Map

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
-function past($h, $m, $s)
+function maps($x)
 {
-    $ms = 0;
-    $ms += $h * 60 * 60 * 1000;
-    $ms += $m * 60 * 1000;
-    $ms += $s * 1000;
-    if ($ms > 0) {
-        return $ms;
-    }else{
-        return 0;
-    }
+  $arr = [];
+  foreach($x as $item){
+    array_push($arr, $item * 2);
+  }
+  return $arr;
 }
-past(0, 1, 1);
+maps([1, 2 ,3]);
